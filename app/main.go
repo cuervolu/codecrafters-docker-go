a4ac066deb0c37b55ad5ab6448d52aab6bdfdf58
cuervolu <angel.cuervo187@gmail.com>
2024-02-21 01:24:22 -0300

    pass stage

--- app/main.go
+++ app/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -12,20 +15,100 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	cmd := exec.Command(command, args...)
+	cmd, err := tempEnv(command, args)
+	if err != nil {
+		fmt.Printf("Error creating temporary environment: %v\n", err)
+		os.Exit(1)
+	}
 
+	if err := cmd.Run(); err != nil {
+		handleRunError(err)
+	}
+}
+
+func tempEnv(command string, args []string) (*exec.Cmd, error) {
+	cmd := exec.Command(command, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	//Create temporary directory
+	dir, err := os.MkdirTemp("", "temp")
 	if err != nil {
-		//Check the status of the error
-		var e *exec.ExitError
-		ok := errors.As(err, &e) // Check if the error is an ExitError
-		if ok {
-			os.Exit(e.ExitCode()) // Exit with the exit code of the command
-		}
-		fmt.Printf("Error: %v\n", err)
+		return nil, err
 	}
+
+	//Copy command to temporary directory
+	if err := CopyFile(command, path.Join(dir, command)); err != nil {
+		return nil, err
+	}
+
+	// Create /dev/null
+	if err := os.MkdirAll("/dev", 0755); err != nil {
+		return nil, err
+	}
+	if devNull, err := os.Create("/dev/null"); err != nil {
+		return nil, err
+	} else {
+		err := devNull.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Chroot to temporary directory
+	if err := syscall.Chroot(dir); err != nil {
+		return nil, err
+	}
+	// Change working directory to /
+	if err := syscall.Chdir("/"); err != nil {
+		return nil, err
+	}
+	return cmd, nil
+}
+
+// CopyFile copies a file from src to dst
+func CopyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer func(srcFile *os.File) {
+		err := srcFile.Close()
+		if err != nil {
+			fmt.Printf("Error closing file: %v\n", err)
+		}
+	}(srcFile)
+
+	// Create the destination file
+	if err := os.MkdirAll(path.Dir(dst), 0755); err != nil {
+		return err
+	}
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer func(dstFile *os.File) {
+		err := dstFile.Close()
+		if err != nil {
+			fmt.Printf("Error closing file: %v\n", err)
+		}
+	}(dstFile)
+	if err := os.Chmod(dst, 0755); err != nil {
+		return err
+	}
+	// Copy the file
+	_, err = io.Copy(dstFile, srcFile)
+	return err
+}
+
+// handleRunError handles errors that occur during command execution
+func handleRunError(err error) {
+	var exitError *exec.ExitError
+	if errors.As(err, &exitError) {
+		os.Exit(exitError.ExitCode())
+	}
+	fmt.Printf("Error executing command: %v\n", err)
+	os.Exit(1)
 }
